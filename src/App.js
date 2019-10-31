1c426c2f829f0c39cc0cd1ac85b3edd8f4cd05db
Michael Goodrum <michael@vlipsy.com>
2019-10-31 01:36:30 -0400

    start repo with first version of site

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Portfolio from './Components/Portfolio';
+import data from './data/resumeData';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData(){
+    this.setState({resumeData: data});
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Footer data={this.state.resumeData.main}/>
+      </div>
+    );
+  }
+}
+
+export default App;
