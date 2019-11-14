e71568c9103b145917bada47b95770cca4a23ec4
Michael Goodrum <michael@vlipsy.com>
2019-11-14 12:37:54 -0500

    fix portfolio broken links

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -7,18 +7,9 @@ class Portfolio extends Component {
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
             <a href={projects.url} title={projects.title} target="_blank">
                <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
-          </div>
         </div>
       })
     }
@@ -30,7 +21,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Here's what I've been working on the past couple years!</h1>
+            <h1>Check Out Some of My Works.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-halfs s-bgrid-thirds cf">
                 {projects}
